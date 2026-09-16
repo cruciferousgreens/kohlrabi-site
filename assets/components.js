2421fd686d331bf2b27b4ae6648d10bcaf1b6695
cruciferousgreens <cruciferousgreens@proton.me>
2026-09-15 20:07:33 -0400

    Swap header brand mark to wheat logo candidate

--- assets/components.js
+++ assets/components.js
@@ -54,7 +54,7 @@
       '<nav class="site-nav" aria-label="Primary navigation">' +
         '<div class="nav-inner">' +
           '<a class="brand" href="' + r + 'index.html" aria-label="Kohlrabi home">' +
-            '<img src="' + r + 'assets/icon-mark.png" alt="" width="96" height="96" />' +
+            '<img src="' + r + 'assets/logo-wheat.png" alt="" width="96" height="96" />' +
             '<span>Kohlrabi</span>' +
           '</a>' +
           '<div class="nav-controls">' +
